b4ddc9e118978fa7f562f3a6397b4c4c4d5a4fad
Pedro Medeiros <pdr.mdrs@gmail.com>
2020-06-13 18:41:40 -0300

    Updates

---
+++ precache-manifest.df05f706337c455a3f755d66003d2621.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634917f6688d4a45db0755277e392860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c15ea376dd32c840722",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6baae4c1e17abeb409c1",
+    "url": "/static/js/2.219b8c7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.219b8c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c15ea376dd32c840722",
+    "url": "/static/js/main.11158210.chunk.js"
+  },
+  {
+    "revision": "ab56b85a88f283a6a026",
+    "url": "/static/js/runtime-main.2780c2bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
